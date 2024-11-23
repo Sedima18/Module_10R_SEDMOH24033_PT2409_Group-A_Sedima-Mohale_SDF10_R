32527b27e8862e10b133948d8e2d25864540e047
Sedima Mohale <sedimamohale18@gmail.com>
2024-11-23 15:04:23 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
+    
+    // Challenge: Refactor the line below into its own function.
+    inputFieldEl.value = ""
+    
+    // Challenge: Refactor the line below into its own function.
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
